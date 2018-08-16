12fd1c378d9f863476e5e34f744fc0b3f7ba5aca
= <alec.hoyland@gmail.com>
2018-08-16 16:58:13 -0400

    Add test script

---
+++ testScript.m
@@ -0,0 +1,14 @@
+% test all the features within the BandwithEstimator class
+
+% load a cell
+import CMBHOME.*
+load('/projectnb/hasselmogrp/Speed Modulation/Caitlins_Cells/Raw/Clamps/CMBobject_clamps-98.mat')
+root.cel = [9, 3];
+
+% create BandwithEstimator object
+best = BandwidthEstimator('Fs', root.fs_video, 'time', root.ts(end), 'spikeTrain', root.cel_i{1});
+
+% rate estimate
+rate = best.rateEstimate('bandwidth', 125);
+figure
+plot(rate / max(vectorise(rate)));
