18e3aab1da20cf12383b53a0ff517a7930464b6a
= <alec.hoyland@gmail.com>
2018-08-16 15:52:16 -0400

    Rewrite computation

--- @BandwidthEstimator/rateEstimate.m
+++ @BandwidthEstimator/rateEstimate.m
@@ -18,15 +18,21 @@ function rate = rateEstimate(self, varargin)
 
   % define important variables
   dt      = 1/self.Fs; % time step
-  nSteps  = self.time(1) : dt : self.time(2); % number of time steps in the series
-  rate    = NaN(length(nSteps), size(self.spikeTrain, 2));
+  steps   = self.time(1) : dt : self.time(2); % number of time steps in the series
+  rate    = NaN(length(steps), size(self.spikeTrain, 2));
 
   [~, normalization] = kernel(1, bandwidth);
 
   for recording = 1:size(self.spikeTrain, 2) % for each spike train in the matrix
-    for step = 1:length(nSteps) % for each time step
-      textbar(step, length(nSteps));
-      rate(step, recording) = normalization * dt * sum(kernel(step - nSteps, bandwidth, true) * self.spikeTrain(:, recording));
+    for step = 1:length(steps) % for each time step in the rate function
+      textbar(step, length(steps));
+      val = 0;
+      for ii = 1:length(steps) % for each time step in the summand
+        if self.spikeTrain(ii, recording) > 0
+          val = val + kernel(step - ii, bandwidth, true) * self.spikeTrain(ii, recording);
+        end
+      end
+      rate(step, recording) = dt / normalization * val;
     end
   end
 
