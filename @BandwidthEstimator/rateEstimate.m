0122ec02e541989884dac19c92702224fbfb42c8
= <alec.hoyland@gmail.com>
2018-08-16 12:15:38 -0400

    Add rate-estimate function for a kernel smoother

---
+++ @BandwidthEstimator/rateEstimate.m
@@ -0,0 +1,14 @@
+function rateEstimate(self, ...)
+  % leave-one-out rate estimate for a kernel smoother
+
+  %% Arguments
+    % self: a BandwidthEstimator object
+    %
+
+  %% Outputs
+    %
+    %
+
+    
+
+end % end function
