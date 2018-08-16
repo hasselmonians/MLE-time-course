aa82ec6005ff598b92abf04e35bebb02d04bbdc1
= <alec.hoyland@gmail.com>
2018-08-16 16:58:02 -0400

    Better way to compute steps vector

--- @BandwidthEstimator/rateEstimate.m
+++ @BandwidthEstimator/rateEstimate.m
@@ -18,14 +18,14 @@ function rate = rateEstimate(self, varargin)
 
   % define important variables
   dt      = 1/self.Fs; % time step
-  steps   = self.time(1) : dt : self.time(2); % number of time steps in the series
+  steps   = 1:length(self.spikeTrain);
   rate    = NaN(length(steps), size(self.spikeTrain, 2));
 
   [~, normalization] = kernel(1, bandwidth);
 
   for recording = 1:size(self.spikeTrain, 2) % for each spike train in the matrix
-    for step = 1:length(steps) % for each time step in the rate function
-      textbar(step, length(steps));
+    parfor step = 1:length(steps) % for each time step in the rate function
+      % textbar(step, length(steps));
       val = 0;
       for ii = 1:length(steps) % for each time step in the summand
         if self.spikeTrain(ii, recording) > 0
