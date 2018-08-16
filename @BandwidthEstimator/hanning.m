04d49705bcf329faf9aacc8e75cded380eb5d130
= <alec.hoyland@gmail.com>
2018-08-16 16:09:40 -0400

    minor fixes

--- @BandwidthEstimator/hanning.m
+++ @BandwidthEstimator/hanning.m
@@ -2,7 +2,7 @@ function [value, normalization] = hanning(x, bandwidth, notch)
   % implements the Hanning kernel (Prerau & Eden 2011)
 
   %% Arguments
-    % x: double, the value at which the kernel should be computed
+    % x: double, the time value at which the kernel should be computed
     % bandwidth: double, the filter length (is used to compute the variance)
     % notch: boolean, if true, kernel(0) = 0, defaults to false
   %% Outputs
@@ -15,12 +15,12 @@ function [value, normalization] = hanning(x, bandwidth, notch)
 
   normalization = x / 2 + (bandwidth - 1) * sin( (2 * pi * x) / (bandwidth - 1) ) / (4 * pi);
 
-  if notch
+  if notch & x == 0
     value = 0;
     return
   end
 
-  if x > -bandwidth / 2 & x <= bandwidth / 2
+  if x > -bandwidth / 2 && x <= bandwidth / 2
     value = 0.5 * (1 + cos( (2*pi*x) / (bandwidth - 1) ) );
   else
     value = 0;
