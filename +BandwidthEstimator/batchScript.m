bf2204ce964dc67e269424b99bff8d63a85485a9
= <alec.hoyland@gmail.com>
2018-08-23 15:47:11 -0400

    Add check for cluster/ location

--- +BandwidthEstimator/batchScript.m
+++ +BandwidthEstimator/batchScript.m
@@ -1,3 +1,12 @@
 % run this script to generate the batchfile
-cd cluster
+
+directory = pwd;
+if ~strcmp(directory(end-6:end), 'cluster')
+  try
+    cd cluster
+  catch
+    error('Error. Can''t find cluster/ directory. Aborting.')
+  end
+end
+
 RatCatcher.batchify('Caitlin', 'A', '/projectnb/hasselmogrp/hoyland/data/caitlin/');
