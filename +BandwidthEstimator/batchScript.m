f6d677f2e0592413c42cec91e234caa71bff4e07
= <alec.hoyland@gmail.com>
2018-08-24 10:57:31 -0400

    minor fixes

--- +BandwidthEstimator/batchScript.m
+++ +BandwidthEstimator/batchScript.m
@@ -6,13 +6,14 @@ arg = RatCatcher.batchify('Caitlin', 'A', '/projectnb/hasselmogrp/hoyland/data/c
 
 if runLocal == true
   for ii = 1:length(arg)
-    strrep(arg, 'false', 'true')
+    strrep(arg, 'false', 'true');
   end
   % create a parallel pool if it doesn't already exist
   pool = gcp;
   % run each of the scripts in the batch files
   for ii = 1:length(arg)
-    textbar(ii, length(arg));
+    textbar(ii, length(arg), '\n');
     disp(['[INFO]' arg{ii} '\n']);
     eval(arg{ii});
+  end
 end
