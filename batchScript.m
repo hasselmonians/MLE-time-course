f5d887dcca3fdfc53f0eedaf5e40ceac45ff502b
= <alec.hoyland@gmail.com>
2018-08-24 15:40:59 -0400

    Run through all of Caitlin's data

--- batchScript.m
+++ batchScript.m
@@ -2,29 +2,54 @@
 
 runLocal = true;
 
-% generate batch files
-arg = RatCatcher.batchify('experimenter', 'Caitlin', 'alpha', 'A', 'analysis', 'BandwidthEstimator', 'location', '/home/ahoyland/code/MLE-time-course/cluster');
+% generate batch files for clusters A-F, simulate and save
 
-% run locally to test things
-if runLocal == true
-  % run only as a test (i.e. do not add to the path)
-  for ii = 1:length(arg)
-    arg = strrep(arg, 'false', 'true');
-  end
-  % create a parallel pool if it doesn't already exist
-  pool = gcp;
-  % run each of the scripts in the batch files
-  for ii = 1:length(arg)
-    disp(['[INFO] ' arg{ii}]);
-    try
-      eval(arg{ii});
-    catch err
-      disp(['[ERROR] ' err.identifier])
+cluster_info_index = ['A' 'B' 'C' 'D' 'E' 'F'];
+for ii = 1:length(cluster_info_index)
+    namespec(ii, :) = ['output-Caitlin-' cluster_info_index(ii)];
+end
+
+for index = 1:length(cluster_info_index)
+
+  experimenter  = 'Caitlin';
+  alpha         = cluster_info_index(index);
+  analysis      = 'BandwidthEstimator';
+  location      = '/home/ahoyland/code/MLE-time-course/cluster';
+
+  arg = RatCatcher.batchify('experimenter', experimenter, 'alpha', alpha, 'analysis', analysis, 'location', location, 'namespec', namespec(index, :));
+
+  % run locally to test things
+  if runLocal == true
+    % run only as a test (i.e. do not add to the path)
+    for ii = 1:length(arg)
+      arg = strrep(arg, 'false', 'true');
     end
+    % create a parallel pool if it doesn't already exist
+    pool = gcp;
+    % run each of the scripts in the batch files
+    for ii = 1:length(arg)
+      disp(['[INFO] ' arg{ii}]);
+      try
+        eval(arg{ii});
+      catch err
+        disp(['[ERROR] ' err.identifier])
+      end
+    end
+    disp(['[INFO] Done!'])
   end
-  disp(['[INFO] Done!'])
+
 end
 
 % gather the data
-dataTable = RatCatcher.gather('location', '/home/ahoyland/code/MLE-time-course/cluster', 'analysis', 'BandwidthEstimator', 'namespec', 'output-');
-dataTable = RatCatcher.stitch('experimenter', 'Caitlin', 'alpha', 'A', 'data', dataTable);
+dataTable = RatCatcher.gather('location', location, 'analysis', analysis, 'namespec', namespec(1, :));
+dataTable = RatCatcher.stitch('experimenter', experimenter, 'alpha', cluster_info_index(1), 'data', dataTable);
+
+for ii = 2:length(cluster_info_index)
+  % create a data table from the next run
+  dataTable2 = RatCatcher.gather('location', location, 'analysis', analysis, 'namespec', namespec(ii, :));
+  dataTable2 = RatCatcher.stitch('experimenter', experimenter, 'alpha', cluster_info_index(ii), 'data', dataTable2);
+  % add that data table to the total
+  dataTable = [dataTable dataTable2];
+end
+
+save('/home/ahoyland/code/MLE-time-course/BandwidthEstimator-Caitlin.mat', 'dataTable');
