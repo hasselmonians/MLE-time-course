9b7965eb14d089cca01aa4efdc8688673faee794
= <alec.hoyland@gmail.com>
2018-08-28 11:29:41 -0400

    Move BandwidthEstimator to new repository

--- +BandwidthEstimator/getFiringRate.m
+++
@@ -1,7 +0,0 @@
-function rate = getFiringRate(root, spikeTrain, bandwidth)
-
-  % wraps the kconv function in a CMBHOME object
-  filter = hanning(bandwidth) / sum(hanning(bandwidth));
-  rate = BandwidthEstimator.kconv(spikeTrain, filter, 1/root.fs_video);
-
-end % function
