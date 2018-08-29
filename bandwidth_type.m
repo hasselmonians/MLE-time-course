0e70fb5e604b97b78f45e4239bbb0d97d79b4ec2
= <alec.hoyland@gmail.com>
2018-08-29 09:46:40 -0400

    cleanup

--- bandwidth_type.m
+++
@@ -1,6 +0,0 @@
-% compare the MLE/CV bandwidth parameter to the type of cell (linear vs. exp)
-
-% load the MLE/CV data
-load('BandwidthEstimator-Caitlin.mat');
-
-% acquire a vector that determines the fitting type (linear vs. saturating exponential) for the firing rate vs. speed curve plot
