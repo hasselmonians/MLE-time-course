1dfd7e009eb1e79bf42813890b790991fb703085
Alec Hoyland <alec.hoyland@posteo.net>
2019-09-19 11:22:21 -0400

    Update script to use new boilerplate code

--- runCaitlinData.m
+++ runCaitlinData.m
@@ -6,5 +6,18 @@ r.project     = 'hasselmogrp';
 r.expID       = {'Caitlin', 'A'; 'Caitlin', 'B'; 'Caitlin', 'C'; 'Caitlin', 'D'; 'Caitlin', 'E'};
 r.verbose     = true;
 
-r = r.batchify();
-save(fullfile(r.localpath, 'Caitlin-BandwidthEstimator-RatCatcher.mat'), 'r')
+return
+
+% batch files
+r = r.batchify;
+
+% NOTE: run the 'qsub' command on the cluster now (see output in MATLAB command prompt)
+
+return
+
+% NOTE: once the cluster finishes, run the following commands
+
+% gather files
+r = r.validate;
+dataTable = r.gather;
+dataTable = r.stitch(dataTable);
