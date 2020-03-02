61e131b36f84349108fbebcd0e8c9de7577dba3b
Alec Hoyland <ahoyland@bu.edu>
2020-03-02 15:32:04 -0500

    Use more specific parsing

--- SpeedModulation/preprocess.m
+++ SpeedModulation/preprocess.m
@@ -1,4 +1,4 @@
 function x = preprocess(x)
-  x = strrep(x, 'projectnb', 'mnt');
-  x = strrep(x, 'hoyland', 'ahoyland');
+  x = strrep(x, '/projectnb', '/mnt');
+  x = strrep(x, '/hoyland', '/ahoyland');
 end % function
