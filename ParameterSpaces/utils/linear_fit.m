1b6660c4c0aa55f4edc1bcb0f22267a0df85faa9
Alec Hoyland <ahoyland@bu.edu>
2020-03-03 13:25:14 -0500

    Allow matrix inputs

--- ParameterSpaces/utils/linear_fit.m
+++ ParameterSpaces/utils/linear_fit.m
@@ -1,3 +1,8 @@
 function y = linear_fit(params, t)
-    y = params(1) + params(2) * t;
+
+    if isvector(params)
+        y = params(1) + params(2) * t;
+    else
+        y = params(:, 1) + params(:, 2) .* t;
+    end
 end % function
--- ParameterSpaces/utils/satexp_fit.m
+++ ParameterSpaces/utils/satexp_fit.m
@@ -1,3 +1,8 @@
 function y = satexp_fit(params, t)
-    y = params(1) - params(2) * exp(- params(3) * t);
+
+    if isvector(params)
+        y = params(1) - params(2) * exp(- params(3) * t);
+    else
+        y = params(:, 1) - params(:, 2) .* exp(- params(:, 3) .* t)
+    end
 end % function
